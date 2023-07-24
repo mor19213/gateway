bf06a13989a95b1305ad975316cd6fe53568e387
Daniela Morales <68123853+mor19213@users.noreply.github.com>
2023-07-24 19:43:53 +0000

    no hice nada de mi trabajo :(

--- my_requests.py
+++ my_requests.py
@@ -6,11 +6,11 @@ class plataformaRequest:
 
     def get(self, nombre):
         response = requests.get(self.url +"/actuador/"+ nombre)
-        print(self.url +"/actuador/"+ nombre)
+        #print(self.url +"/actuador/"+ nombre)
         return response
 
     def put(self, nombre, data):
-        print(self.url + "/sensor/"+ nombre)
+        #print(self.url + "/sensor/"+ nombre)
         response = requests.put(self.url+"/sensor/" + nombre, json=data)
         return response
     
@@ -19,6 +19,6 @@ class plataformaRequest:
         return response
 
     def put_dev(self, nombre, data):
-        print("http://127.0.0.1:8000/mor19213/sensor/"+ nombre)
+        #print("http://127.0.0.1:8000/mor19213/sensor/"+ nombre)
         response = requests.put("http://127.0.0.1:8000/mor19213/sensor/"+ nombre, json=data)
         return response
--- udp/udp_messages.py
+++ udp/udp_messages.py
@@ -1,15 +1,16 @@
-# program to recieve and send udp messages
-import socket
-import threading
-
 import sys
 sys.path.append("..")
 from my_requests import plataformaRequest
+import socket
+import threading
+import time
 
 RCV_IP = "0.0.0.0"
 RCV_PORT = 5005
-SEND_IP = "192.168.0.122"
-SEND_PORT = 5005
+TEST_IP = "172.16.5.4"
+TEST_PORT = 5005
+SEND_PORT = 50
+actuadores = []
 Request = plataformaRequest("mor19213")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -30,22 +31,28 @@ def recieve_message():
             data = {"valor": valor}
             if tipo == "sensor":
                 my_request = Request.put(name, data)
+                if my_request.status_code == 200:
+                    print("actualizado")
             elif tipo == "actuador":
-                my_request = Request.get(name)
+                # add actuador to list if not already in it
+                if valor == "desconectar":
+                    actuador = next((actuador for actuador in actuadores if actuador["nombre"] == name), None)
+                    print("actuador a eliminar: "+actuador["nombre"])
+                    actuadores.remove(actuador)
+                else:
+                    if not any(actuador["nombre"] == name for actuador in actuadores):
+                        actuadores.append({"nombre": name, "ip": addr[0]})
+                print(actuadores)
+            elif tipo == "test":
+                print(msg)
             else:
-                continue
-            if my_request.status_code == 200:
-                print("actualizado")
-                data = my_request.json()
-                data = str({'valor': str(data["valor"])})
                 print(data)
-            else:
-                print(my_request)
 
-        except KeyboardInterrupt:
-            print("Keyboard Interrupt recieve")
-            break
+        except:
+            print("Error al recibir")
+            continue
     sock.close()
+
 
 recieve_thread = threading.Thread(target=recieve_message)
 recieve_thread.start()
@@ -54,13 +61,41 @@ send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 def send_message():
     while True:
-        try:
-            message = input("")
-            send_socket.sendto(message.encode("utf-8"), (SEND_IP, SEND_PORT))
-        except KeyboardInterrupt:
-            print("Keyboard Interrupt send")
-            break
+        # wait 4 seconds
+        time.sleep(4)
+        for actuador in actuadores:
+            try:
+                my_request = Request.get(actuador["nombre"])
+                if my_request.status_code == 200:
+                    print("actualizado")
+                    data = my_request.json()
+                    data = str({'valor': str(data["valor"])})
+                    data = "test/"+actuador["nombre"]+"/"+data
+                    send_socket.sendto(data.encode("utf-8"), (actuador["ip"], TEST_PORT))
+                    ##send_socket.sendto(data.encode("utf-8"), (actuador["ip"], SEND_PORT))
+                else:
+                    # remove actuador from list
+                    actuadores.remove(actuador)
+                    print("Actuador removido")
+
+            except:
+                print("Error al enviar")
+                continue
     send_socket.close()
 
-send_message()
-recieve_thread.join()
+send_thread = threading.Thread(target=send_message)
+send_thread.start()
+
+test_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def send_test():
+    while True:
+        try: 
+            message = input("")
+            test_socket.sendto(message.encode("utf-8"), (TEST_IP, TEST_PORT))
+        except:
+            print("error test")
+            continue
+    test_socket.close()
+
+send_test_thread = threading.Thread(target=send_test)
+send_test_thread.start()
